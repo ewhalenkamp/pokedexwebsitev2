9bd4b05bed3dbf50d402dd24ba3b9cf70185b2df
ewhalenkamp <70407222+ewhalenkamp@users.noreply.github.com>
2021-03-23 13:49:40 -0600

    static root again

--- pokedexwebsitev2/settings.py
+++ pokedexwebsitev2/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/pokedexwebsitev2/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'pokedexwebsitev2/static/'
